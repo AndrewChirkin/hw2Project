baffbf48a6260f8fbee3d16945409bf163ffdf3a
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-08-25 22:02:57 +0300

    hw2pt6

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-    var frog = 3.5;
-    System.out.println(frog);
-    frog = frog * 10;
-    System.out.println(frog);
-    frog = frog / 3.5;
-    System.out.println(frog);
-    frog = frog + 4;
-    System.out.println(frog);
+    var boxer1Weight = 78.2;
+    var boxer2Weight = 82.7;
+    var totalWeight = boxer1Weight + boxer2Weight;
+    System.out.println("Общий вес боксёров:" + totalWeight + "кг");
+    var weightDifference = boxer2Weight - boxer1Weight;
+    System.out.println("Разница в весе боксёров:" + weightDifference + "кг");
+
 
     }
 }
